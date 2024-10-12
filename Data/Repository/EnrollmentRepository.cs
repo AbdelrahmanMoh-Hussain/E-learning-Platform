3b8ae914ff93e7e8c43451d6fe059adbf5b8d98f
Abdelrahman-Mohamed <abdelrahman.mohamed7337@gmail.com>
2024-10-12 14:38:30 +0300

    Cart functionality

---
+++ Data/Repository/EnrollmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using E_learning_Platform.Data.Repository.Interfaces;
+using E_learning_Platform.Models;
+
+namespace E_learning_Platform.Data.Repository
+{
+    public class EnrollmentRepository : IEnrollmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnrollmentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Enrollement> AddEnrollments(int userId, List<Course> courses)
+        {
+            List<Enrollement> enrollements = new List<Enrollement>();
+            foreach (var course in courses)
+            {
+                var enrollment = new Enrollement
+                {
+                    UserId = userId,
+                    CourseId = course.Id
+                };
+                _context.Enrollement.Add(enrollment);
+                var effectedRows = _context.SaveChanges();
+                if(effectedRows > 0)
+                {
+                    enrollements.Add(enrollment);
+                }
+            }
+            return enrollements;
+
+        }
+    }
+}
---
+++ Data/Repository/Interfaces/IEnrollmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using E_learning_Platform.Models;
+
+namespace E_learning_Platform.Data.Repository.Interfaces
+{
+    public interface IEnrollmentRepository
+    {
+        public IEnumerable<Enrollement> AddEnrollments(int userId, List<Course> courses);
+    }
+}
